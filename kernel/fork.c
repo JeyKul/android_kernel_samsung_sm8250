74d79f829a4b579a9b166d297b1ecec3052fa09b
ata-kaner <ata.kaner03@gmail.com>
2024-07-11 14:11:31 +0300

    r8q_eur: Import kernel/ changes
    Change-Id: Iefa8d36ad3765a76360ffbd62ca89fd1f00c2ae4

--- kernel/fork.c
+++ kernel/fork.c
@@ -108,6 +108,10 @@
 #define CREATE_TRACE_POINTS
 #include <trace/events/task.h>
 
+#ifdef CONFIG_SECURITY_DEFEX
+#include <linux/defex.h>
+#endif
+
 /*
  * Minimum number of threads to boot the kernel
  */
@@ -1648,6 +1652,16 @@ static void posix_cpu_timers_init(struct task_struct *tsk)
 static inline void posix_cpu_timers_init(struct task_struct *tsk) { }
 #endif
 
+#ifdef CONFIG_KDP_CRED
+void rkp_assign_pgd(struct task_struct *p)
+{
+	u64 pgd;
+	pgd = (u64)(p->mm ? p->mm->pgd :swapper_pg_dir);
+
+	uh_call(UH_APP_RKP, RKP_KDP_X43, (u64)p->cred, (u64)pgd, 0, 0);
+}
+#endif
+
 static inline void init_task_pid_links(struct task_struct *task)
 {
 	enum pid_type type;
@@ -2247,6 +2261,10 @@ static __latent_entropy struct task_struct *copy_process(
 
 	trace_task_newtask(p, clone_flags);
 	uprobe_copy_process(p, clone_flags);
+#ifdef CONFIG_KDP_CRED
+	if(rkp_cred_enable)
+		rkp_assign_pgd(p);
+#endif
 
 	copy_oom_score_adj(clone_flags, p);
 
@@ -2394,6 +2412,10 @@ long _do_fork(unsigned long clone_flags,
 
 	pid = get_task_pid(p, PIDTYPE_PID);
 	nr = pid_vnr(pid);
+
+#ifdef CONFIG_SECURITY_DEFEX
+	task_defex_zero_creds(p);
+#endif
 
 	if (clone_flags & CLONE_PARENT_SETTID)
 		put_user(nr, parent_tidptr);
